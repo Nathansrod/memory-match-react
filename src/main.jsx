e903d2b55f46caef3898fddf4f0d30905e387914
Nathansrod <nathan.silva.rodrigues@gmail.com>
2024-04-23 19:50:22 -0300

    browser router #2

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import BrowserRouter from 'react-router-dom/BrowserRouter';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter basename={process.env.PUBLIC_URL}>
-     <App />
-   </BrowserRouter>,
-)
+    <App />
+  </BrowserRouter>
+);
