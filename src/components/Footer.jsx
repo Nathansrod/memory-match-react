8deb0abd6aba55b08af9ee7daf7431b7db9fcf39
Nathansrod <nathan.silva.rodrigues@gmail.com>
2024-04-23 19:29:49 -0300

    github pages config

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,16 @@ export default function Footer() {
   return (
     <footer className="mt-8 text-sm text-stone-50">
       <div>
+        Made by{" "}
+        <a
+          className="font-bold underline"
+          href="https://www.linkedin.com/in/nathan-silva-rodrigues-7ab0131b7/"
+        >
+          Nathan S. Rodrigues
+        </a>
+        , April 2024
+      </div>
+      <div className="mt-4">
         <a
           className="font-bold underline"
           href="https://www.freepik.com/free-vector/fruits-collection-cartoon-vector-icon-illustration-food-nature-icon-concept-isolated-premium-vector_46969946.htm#query=fruit%20collection&position=16&from_view=keyword&track=ais&uuid=555de073-ad1b-49a7-9370-8584eb9e5112"
